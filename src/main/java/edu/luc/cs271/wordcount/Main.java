27f89e074e2d28d61ed6a51c0dee7f0d58fcb30f
linmaliakal <lmaliakal@luc.edu>
2017-11-08 04:32:11 +0000

    completed TODO items in Main
    Signed-off-by: linmaliakal <lmaliakal@luc.edu>

--- src/main/java/edu/luc/cs271/wordcount/Main.java
+++ src/main/java/edu/luc/cs271/wordcount/Main.java
@@ -9,7 +9,7 @@ public class Main {
     // set up the scanner so that it separates words based on space and punctuation
     final Scanner input = new Scanner(System.in).useDelimiter("[^\\p{Alnum}]+");
 
-    // TODO complete this main program
+    // DONE complete this main program
     // 1. create a WordCounter instance
     // 2. use this to count the words in the input
     // 3. determine the size of the resulting map
