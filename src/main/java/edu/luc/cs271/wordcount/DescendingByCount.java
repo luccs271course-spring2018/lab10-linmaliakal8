0d934d5e7132102a6595e21857f3095e4ae82669
linmaliakal <lmaliakal@luc.edu>
2017-11-08 04:15:13 +0000

    completed comparing and descending order of entries
    Signed-off-by: linmaliakal <lmaliakal@luc.edu>

--- src/main/java/edu/luc/cs271/wordcount/DescendingByCount.java
+++ src/main/java/edu/luc/cs271/wordcount/DescendingByCount.java
@@ -7,7 +7,7 @@ import java.util.Map;
 public class DescendingByCount implements Comparator<Map.Entry<String, Integer>> {
   public int compare(final Map.Entry<String, Integer> l, final Map.Entry<String, Integer> r) {
 
-    // TODO turn this into descending order using the getValue() method on l and r
+    // DONE turn this into descending order using the getValue() method on l and r
     return l.getValue().compareTo(r.getValue());
 
   }
