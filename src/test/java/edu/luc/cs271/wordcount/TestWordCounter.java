854d98ab7618cdafdafe36325de4baa0743e14d7
linmaliakal <lmaliakal@luc.edu>
2017-11-08 05:24:38 +0000

    fixed parts of testing
    Signed-off-by: linmaliakal <lmaliakal@luc.edu>

--- src/test/java/edu/luc/cs271/wordcount/TestWordCounter.java
+++ src/test/java/edu/luc/cs271/wordcount/TestWordCounter.java
@@ -2,22 +2,23 @@ package edu.luc.cs271.wordcount;
 
 import static org.junit.Assert.*;
 
-import java.util.Map;
-import java.util.HashMap;
-import java.util.Collections;
-import java.util.Arrays;
-import java.util.Iterator;
+//import java.util.Map;
+//import java.util.HashMap;
+//import java.util.Collections;
+//import java.util.Arrays;
+//import java.util.Iterator;
+import java.util.*;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
 public class TestWordCounter {
 
-  // TODO complete this test class
+  // DONE complete this test class
 
   // DONE declare a reference to the SUT (system under test), i.e., WordCounter
+  
   WordCounter counter;
-  Map<String, Integer> testMap;
 
   @Before
   public void setUp() {
@@ -46,16 +47,14 @@ public class TestWordCounter {
     // DONE run the SUT on a specific String iterator with some repeated words,
     // then use assertions to verify the correct counts
     // do this for at least two words in the iterator and two not in the iterator
-    Iterator<String> testList = Arrays.asList("It", "was", "a", "dark", "and", "stormy", "night", "dark", "stormy", "night").iterator();
     
-    WordCounter counter = new WordCounter(testMap);
-    counter.countWords(testList);
-    //counter.countWords(Arrays.asList("It", "was", "a", "dark", "and", "stormy", "night", "dark", "stormy", "night").iterator());
+    ArrayList<String> testList = new ArrayList<String>(Arrays.asList("It", "was", "a", "dark", "and", "stormy", "night", "dark", "stormy", "night"));
+    counter.countWords(testList.iterator());
 
     //test fails, look over
     assertEquals(counter.getCount("dark"), 2);
     assertEquals(counter.getCount("was"), 1);
-    //assertNotEquals(counter.getCount("light"), 1);
-    //assertNotEquals(counter.getCount("day"), 1);
+    assertEquals(counter.getCount("light"), 0);
+    assertEquals(counter.getCount("day"), 0);
   }
 }
